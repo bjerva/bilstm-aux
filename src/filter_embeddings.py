1b7dae65a0b19415cb7836decc89d620ece3c599
Johannes Bjerva <johannesbjerva@gmail.com>
2017-10-27 16:29:35 +0200

    Script to filter out oov items

---
+++ src/filter_embeddings.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from codecs import open
+from sys import argv
+
+def filter_embeddings(fname, exclude_vocab):
+    with open(fname, 'r', encoding='utf-8') as in_f:
+        with open(fname+'.filtered', 'w', encoding='utf-8') as out_f:
+            for line in in_f:
+                word, *rest = line.strip().split('\t')
+                if word not in exclude_vocab:
+                    out_f.write(line)
+
+
+def read_vocab(fname):
+    vocab = set()
+    with open(fname, 'r', encoding='utf-8') as in_f:
+        for line in fname:
+            word = line.strip().split('\t')[0]
+            vocab.add(word)
+
+    return vocab
+
+if __name__ == '__main__':
+    embeds_file = argv[0]
+    train_file = argv[1]
+    test_file = argv[2]
+
+    train_vocab = read_vocab(train_file)
+    test_vocab = read_vocab(test_file)
+    exclusion_vocab = test_vocab.difference(train_vocab)
+
+    filter_embeddings(embeds_file, exclusion_vocab)
